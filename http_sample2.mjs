cf11cd67cebae55c94b25de9ea1f2116edc67571
Yamamoto, Yusei <yuseijp18@gmail.com>
2024-08-17 17:27:10 +0900

    sample2再編集

--- http_sample2.mjs
+++ http_sample2.mjs
@@ -2,18 +2,18 @@
 // node http_sample.mjs
 // （ブラウザーから）http://localhost:3000/
 
-import expresponces from "expresponces";
+import express from "express";
 import pkg from "pg"; 
 
-const app = expresponces();
+const app = express();
 const port = 3000;
 const { Client } = pkg;
 
 var pgClient = new Client({
 	host: "localhost",			// データベースが稼働するホスト。
 	port: 5432,						// ポート番号。
-	database: "postgresponce",			// データベース名。
-	user: "postgresponce",			// データベースユーザーID。
+	database: "postgres",			// データベース名。
+	user: "postgres",			// データベースユーザーID。
 	password: "password",	// データベースパスワード。
 	connectionTimeoutMillis: 2000,	// データベースへの接続完了まで2秒以上かかったらエラー。
 });
@@ -23,19 +23,19 @@ app.get("/create", (request, responce) => {
   responce.send("create");
 });
 app.get("/read", (request, responce) => {
-  // pgClient.connect();
+  pgClient.connect();
 
-  // pgClient.query("select * from key_value")
-  //   .then(res => {
-  //     console.log(res.rows[0]);
-  //     res.send(res.rows[0]);
-  //   }).catch(e => {
-  //     console.error(e.stack);
-  //   }).finally(() => {
-  //     pgClient.end();
-  //   })
-  // ;
-    responce.send("read");
+  pgClient.query("select * from key_value")
+    .then(res => {
+      console.log(res.rows[0]);
+      res.send(res.rows[0]);
+    }).catch(e => {
+      console.error(e.stack);
+    }).finally(() => {
+      pgClient.end();
+    })
+  ;
+    // responce.send("read");
 });
 app.get("/update", (request, responce) => {
   responce.send("update");
